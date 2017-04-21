2fad42c0e70661f92711b417c4f9c48488bb08db
Jake Sorce <jakesorce@gmail.com>
2017-04-21 12:55:24 -0600

    remove image and sanatize nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
 end
