1fc0081e64128cb093c5ea97f546d1d4deb09604
Jake Sorce <jakesorce@gmail.com>
2017-04-21 13:06:56 -0600

    override reg controller

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Api::RegistraionsController < DeviseTokenAuth::RegistrationsController
+  def create
+    super do |resource|
+      sign_in(resource)
+      res = resource.create_new_auth_token
+      render json: {
+        id: resource.id,
+        email: resource.email,
+        nickname: resource.nickname,
+        client: res['client'],
+        token: res['access-token']
+      } and return
+    end
+  end
+end
